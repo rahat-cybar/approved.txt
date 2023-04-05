be45700978daea322505ae72cd5b0d83deb09152
rahat-cybar <126415254+rahat-cybar@users.noreply.github.com>
2023-04-05 16:43:04 +0600

    paid py

--- paid.py
+++ paid.py
@@ -415,12 +415,12 @@ class Main:
 		print("\033[1;33m [2] Exit")
 		print("")
 		RAHAT = input("\n\033[1;36m  Chose ==> \033[1;32m")
-		if RAHAT in ["", " "]:
+		if  RAHAT in ["", " "]:
 			exit()
-		elif RAHAT in ["2", "02"]:
+		elif  RAHAT in ["2", "02"]:
 			print("    Thanks🥰♥️")
 			exit()
-		elif RAHAT in ["1", "01"]:
+		elif  RAHAT in ["1", "01"]:
 			os.system("xdg-open https://youtu.be/gxjhqkJL_h0")
 			print("")
 			time.sleep(2.0)
